1c171b8d5da4e4bd347b59c88b0c7ed524c406b8
DyepLucban <lucbanjep@gmail.com>
2021-03-29 21:39:00 +0800

    initial commit

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::group(['prefix' => 'tasks'], function() {
     Route::get('/', 'TaskController@index');
     Route::get('/{id}', 'TaskController@browseSpecific');
